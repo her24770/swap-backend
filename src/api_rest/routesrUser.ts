c52ef7be793921640d586b09f24c5da4733051a4
stan-2021131 <sergiocoromac@gmail.com>
2026-04-07 19:47:21 -0600

    HU-05 creación de rutas para manejo de usuarios y sus atributos

--- src/api_rest/routesrUser.ts
+++ src/api_rest/routesrUser.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
+import { obtenerUsuario, actualizarPerfil, agregarContacto, obtenerContactos } from "../controlador/controlUsuario.js";
 
 const router = Router();
 
 //Ruta para obtener datos del usuario así como sus publicaciones
-router.get("/:id", (_req, res) => {
-    res.json({ message: "Users" });
-});
+router.get("/:id", obtenerUsuario);
 
 //Ruta para actualizar datos del usuario
-router.patch("/:id", (_req, res) => {
-    res.json({ message: "Users" });
-});
+router.patch("/:id", actualizarPerfil);
+
+//Ruta para agregar un contacto al usuario
+router.post("/contacto/:id", agregarContacto);
+
+//Ruta para obtener los contactos del usuario
+router.get("/contactos/:id", obtenerContactos);
 
 export default router;
