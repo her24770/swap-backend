2efbe22c0c645b20910619ba97fb3bf03d99cdd6
stan-2021131 <sergiocoromac@gmail.com>
2026-04-14 00:15:00 -0600

    Pequeña corrección en schema zod de registro, implementación del servicio de bcrypt en login y registro, y integración de middleware de validación de consultas para las rutas de registro y login

--- src/api_rest/routerAuth.ts
+++ src/api_rest/routerAuth.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import { validar } from "../autenticacion/middelwareValidacion.js";
+import { schemaRegistro, schemaLogin } from "../modelo/schemaAuth.js";
 import { registro, iniciarSesion } from "../controlador/controlAuth.js";
 
 const router = Router();
 
 //Ruta para registrar un nuevo usuario
-router.post("/register", registro);
+router.post("/register", validar(schemaRegistro), registro);
 
 //Ruta para iniciar sesión
-router.post("/login", iniciarSesion);
+router.post("/login", validar(schemaLogin), iniciarSesion);
 
 export default router;
