7842df5749ac331870a6054cd0811a161493850e
stan-2021131 <sergiocoromac@gmail.com>
2026-04-17 18:19:39 -0600

    Se agregó repositorio tipos de perfil para su uso en validaciones

---
+++ src/repository/repositorioTipoPerfil.ts
@@ -0,0 +1,23 @@
+import { TipoPerfil } from "@prisma/client";
+import prisma from "../persistencia/prismaClient";
+
+/**
+ * Obtener todos los tipos de perfil
+ */
+export async function obtenerTiposPerfil(): Promise<TipoPerfil[] | []> {
+    return await prisma.tipoPerfil.findMany();
+}
+
+/**
+ * Obtener un tipo de perfil por su id
+ */
+export async function obtenerTipoPerfilPorId(id: number): Promise<TipoPerfil | null> {
+    return await prisma.tipoPerfil.findUnique({ where: { id_tipo_perfil: id } });
+}
+
+/**
+ * Obtener un tipo de perfil por su nombre
+ */
+export async function obtenerTipoPerfilPorNombre(nombre: string): Promise<TipoPerfil | null> {
+    return await prisma.tipoPerfil.findUnique({ where: { tipo_perfil: nombre } });
+}
