d644274aa143c65d3896747222a84953cd65b51a
Jackelyn <jackelyngironvillacinda@gmail.com>
2026-05-24 03:02:46 -0600

    Cambio para ver etiquetas sin necesitar autenticacion

--- src/api_rest/routesEtiqueta.ts
+++ src/api_rest/routesEtiqueta.ts
@@ -6,6 +6,6 @@ const router = Router();
 
 router.get("/user/:id", autenticar, obtenerEtiquetasUsuario); //Ruta para las etiquetas asignadas a un usuario
 router.get("/publicacion/:id", autenticar, obtenerEtiquetasPublicacion); //Ruta para las etiquetas asignadas a una publicacion
-router.get("/", autenticar, obtenerEtiquetas);
+router.get("/", obtenerEtiquetas);
 
 export default router;
