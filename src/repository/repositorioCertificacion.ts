d0c3f3eced0d28b651fa565dd1d9dde33f4932f5
her24770 <her24770@uvg.edu.gt>
2026-05-26 01:24:33 -0600

    agregar repositorio con operaciones de BD para certificaciones

---
+++ src/repository/repositorioCertificacion.ts
@@ -0,0 +1,35 @@
+import { Certificacion } from "@prisma/client";
+import prisma from "../persistencia/prismaClient";
+
+export async function crearCertificacion(data: {
+    id_usuario: number;
+    nombre: string;
+    ruta_pdf: string;
+    lugar_emision: string;
+    id_etiqueta: number;
+}): Promise<Certificacion> {
+    return prisma.certificacion.create({ data });
+}
+
+export async function eliminarCertificacion(id_certificacion: number): Promise<Certificacion> {
+    return prisma.certificacion.delete({ where: { id_certificacion } });
+}
+
+export async function buscarCertificacionesPorUsuario(id_usuario: number) {
+    return prisma.certificacion.findMany({
+        where: { id_usuario },
+        include: { etiqueta: true },
+        orderBy: { id_certificacion: "desc" },
+    });
+}
+
+export async function buscarCertificacionPorId(id_certificacion: number) {
+    return prisma.certificacion.findUnique({
+        where: { id_certificacion },
+        include: { etiqueta: true, usuario: { select: { id_usuario: true, nombre: true, url_foto_perfil: true } } },
+    });
+}
+
+export async function contarCertificacionesPorUsuario(id_usuario: number): Promise<number> {
+    return prisma.certificacion.count({ where: { id_usuario } });
+}
