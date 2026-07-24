03928c85da871a884a736658e32c44957223deaa
stan-2021131 <sergiocoromac@gmail.com>
2026-07-23 18:34:06 -0600

    feat: Schema zod para la creación de solicitud de acuerdo

---
+++ src/modelo/schemaAcuerdo.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const solicitudAcuerdoSchema = z.object({
+    fecha_entrega: z.coerce.date(),
+    lugar_entrega: z.string().min(1, "El lugar de entrega es requerido"),
+    observaciones: z.string().min(1, "Las observaciones son requeridas"),
+    id_conversacion: z.number().min(1, "La conversacion es requerida")
+});
