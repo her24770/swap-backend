42649ece8688951b9bf6008b4d9db0a379a42914
Oscar Rompich <oscarligo6@gmail.com>
2026-07-26 02:05:21 -0600

    feat: flujo para obtener las conversaciones de un usuario

--- src/api_rest/routesConversacion.ts
+++ src/api_rest/routesConversacion.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { autenticar } from "../autenticacion/GestorPermisos.js";
-import { actualizarEstadoConversacion } from "../controlador/controlConversacion.js";
+import { actualizarEstadoConversacion, obtenerConversacionesDeUsuario } from "../controlador/controlConversacion.js";
 
 const router = Router();
 
-router.put("/:id/estado", autenticar, actualizarEstadoConversacion); //Ruta para bloquear o aceptar la solicitud de conversacion
+router.put("/:id/estado", autenticar, actualizarEstadoConversacion) //Ruta para bloquear o aceptar la solicitud de conversacion
+router.get("/conversaciones", autenticar, obtenerConversacionesDeUsuario) 
 
 export default router;
