3a668985f8d623a348b4370290dc57955cd08426
Oscar Rompich <oscarligo6@gmail.com>
2026-09-04 01:49:30 -0600

    fix: obtener los acuerdos por medio del id en el token de un usuario.

--- src/api_rest/routesAcuerdo.ts
+++ src/api_rest/routesAcuerdo.ts
@@ -7,7 +7,7 @@ import { actualizarAcuerdoSchema, actualizarEstadoAcuerdoSchema, solicitudAcuerd
 
 const router = Router();
 
-router.get("/user/:id", autenticar, obtenerAcuerdosUsuario); //Ruta para los acuerdos recibidos por el usuario
+router.get("/", autenticar, obtenerAcuerdosUsuario); //Ruta para los acuerdos recibidos por el usuario
 router.get("/conversacion/:id", autenticar, obtenerAcuerdosConversacion); //Ruta para los acuerdos asociados a una conversacion
 
 router.post("/:id", autenticar, soloUsuario, validar(solicitudAcuerdoSchema), crearSolicitarAcuerdo); //Ruta para crear una solicitud de acuerdo (id: idPublicacion)
